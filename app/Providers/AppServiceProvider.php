bc01d62693dfa99f235ad326bdd21feacab7bdcc
Zair Duarte <zairduarte22@gmail.com>
2026-07-09 12:30:59 -0400

    feat: configure application routing, middleware, and custom public path binding

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,7 +12,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind('path.public', function() {
+            return base_path('../public_html');
+        });
     }
 
     /**
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -26,4 +26,4 @@ return Application::configure(basePath: dirname(__DIR__))
         $exceptions->shouldRenderJsonWhen(
             fn (Request $request) => $request->is('api/*'),
         );
-    })->create();
+    })->create()->usePublicPath(__DIR__.'/../../public_html');
