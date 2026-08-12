724928727e052869aa548e46d4dc6abea90b9b6e
zairduarte22 <zairduarte22@gmail.com>
2026-08-12 08:49:15 -0400

    feat: create IngresoHistorico model and database migration

---
+++ database/migrations/2026_08_12_084852_create_ingreso_historicos_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ingresos_historicos', function (Blueprint $table) {
+            $table->id();
+            $table->date('fecha');
+            $table->decimal('monto', 15, 2);
+            $table->string('descripcion')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ingreso_historicos');
+    }
+};
