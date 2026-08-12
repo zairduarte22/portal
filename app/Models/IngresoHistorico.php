0c1b6d669f622a27751c4820422bb980fedd1bed
zairduarte22 <zairduarte22@gmail.com>
2026-08-12 08:49:28 -0400

    feat: create ingresos_historicos table migration

--- app/Models/IngresoHistorico.php
+++ app/Models/IngresoHistorico.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class IngresoHistorico extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'ingresos_historicos';
+    protected $fillable = ['fecha', 'monto', 'descripcion'];
 }
--- database/migrations/2026_08_12_084852_create_ingreso_historicos_table.php
+++ database/migrations/2026_08_12_084852_create_ingreso_historicos_table.php
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('ingreso_historicos');
+        Schema::dropIfExists('ingresos_historicos');
     }
 };
